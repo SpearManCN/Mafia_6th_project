dce71ad2e31eb00fdbdfb505806cb6883fe79dd8
SpearManCN <119284822+SpearManCN@users.noreply.github.com>
2024-04-27 23:54:04 +0900

    24.04.27 1st commit

--- src/main/java/mafia/Config/SecurityConfig.java
+++ src/main/java/mafia/Config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package mafia.Config;
 
+import mafia.Service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -21,11 +27,13 @@ public class SecurityConfig {
 //    public WebSecurityCustomizer webSecurityCustomizer() {
 //        return (web) -> web.ignoring().requestMatchers("/login");
 //    }
+    @Autowired
+    private CustomUserDetailService userDetailsService;
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/WEB-INF/views/login.jsp"
+                .requestMatchers("/WEB-INF/views/login.jsp","/*/**"
 //                        "/loginProc","/ranking","/login","/public/login", "/css/**", "/images/**", "/js/**"
                 )
                 .permitAll()
@@ -34,29 +42,25 @@ public class SecurityConfig {
         http
                 .formLogin()
                 .loginPage("/login")
-//                .loginProcessingUrl("/loginProc")
-//                .defaultSuccessUrl("/ranking")
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/ranking")
+                .permitAll();
+        http
+                .logout()
+                .logoutUrl("/logoutProc")
+                .logoutSuccessUrl("/ranking")
                 .permitAll();
 
-                //.loginProcessingUrl("/loginProc");
-//                //.successHandler(로그인 성공 시 실행할 커스터마이즈드 핸들러)
-//                //.failureHandler(로그인 실패 시 실행할 커스터마이즈드 핸들러);
-//
-//        http
-//                .sessionManagement()
-//                .invalidSessionUrl("/login")
-//
-//                .and()
-//                .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logoutProc"))
-//                .invalidateHttpSession(true)
-//                .deleteCookies("JSESSIONID")
-//                .permitAll();
-//
-//
-//        //CSRF 토큰
-//        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+        //return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
